b10fbdb8e3a71955e3c4da68dcdf4dcfd6d87092
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-02 17:17:44 +0200

    giradischi: Remove dependency to sebaubuntu_libs
    Change-Id: Iebd99b51db8220ec7589e5145f681798dd012998

--- giradischi/main.py
+++ giradischi/main.py
@@ -4,11 +4,7 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 #
 
-from sebaubuntu_libs.liblogging import setup_logging
-
 from giradischi.ui import GiradischiUI
 
 def main():
-	setup_logging()
-
 	GiradischiUI().start_ui()
