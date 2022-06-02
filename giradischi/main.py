f0fcbad2842d04b7fc43102a965900b10434a0be
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-02 17:46:38 +0200

    giradischi: Implement command line file opening
    Change-Id: Ifed25eb7df8ef650840d1cffe452f8a7e4b46628

--- giradischi/main.py
+++ giradischi/main.py
@@ -4,7 +4,13 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 #
 
+from argparse import ArgumentParser
 from giradischi.ui import GiradischiUI
+from pathlib import Path
 
 def main():
-	GiradischiUI().start_ui()
+	parser = ArgumentParser(description='Giradischi')
+	parser.add_argument('file', nargs='?', default=None, help='File to open', type=Path)
+	args = parser.parse_args()
+
+	GiradischiUI(args.file).start_ui()
